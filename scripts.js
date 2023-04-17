f3f17c5945d019cc194518e80049cbc00c10e76a
Kuldysak <23kuldys23@gmail.com>
2023-04-17 14:46:33 +0200

    kekw new activity

--- scripts.js
+++ scripts.js
@@ -12,14 +12,14 @@
     var distance = now - countDownDate;
 
     // výpočet dnů, hodin, minut a sekund
-    var months = Math.floor(distance / (1000 * 60 * 60 * 24 * (365.25/12)));
+    var months = Math.floor(distance / (1000 * 60 * 60 * 24 * (365.25 / 12)));
     var days = Math.floor(distance / (1000 * 60 * 60 * 24) - 30 * months);
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
     //skloňování 
-    if (months == 2) {
+    if (months == 2 || months == 3 || months == 4) {
         mm = " Měsíce ";
     } else if (months == 1) { 
         mm = " Měsíc ";
@@ -27,25 +27,12 @@
         mm = " Měsíců "
     }
 
-
     if (days == 2 || days == 3 || days == 4) {
         dd = " Dny ";
     } else if (days == 1) { 
         dd = " Den ";
     } else {
         dd = " Dní "
-    }
-
-    if (hours > 1) {
-        hh = " Hodin ";
-    } else {
-        hh = " Hodinu ";
-    }
-
-    if (minutes > 1) {
-        mmm = " Minut ";
-    } else {
-        mmm = " Minutu ";
     }
 
     // vypsání výsledku do elementu na stránce
